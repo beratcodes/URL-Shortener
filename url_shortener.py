b78993d6eb0a89c0b1ac355549efdd9becb8e542
beratcodes <159455724+beratcodes@users.noreply.github.com>
2025-12-30 16:05:53 +0300

    '.exe' is created.

--- url_shortener.py
+++ url_shortener.py
@@ -3,7 +3,6 @@
 # Author  : beratcodes
 # Date    : 2025-12-30
 # ----------------------------------------
-
 import tkinter as tk
 from tkinter import messagebox
 import requests
@@ -23,20 +22,25 @@ def copy_to_clipboard():
     messagebox.showinfo("Copied","Short URL is copied.")
 
 # GUI
+# .ico file
+photo = tk.PhotoImage(file= "url.ico")
+
 app_window = tk.Tk()
 app_window.title("URL Shortener")
 app_window.resizable(0,0)
+app_window.iconphoto(True, photo)
 
+# Entry, Label, Buttons
 long_label = tk.Label(app_window, text="Insert Long Link:")
 long_entry = tk.Entry(app_window, width=40)
 result_label = tk.Label(app_window, text="")
 shorten_button = tk.Button(app_window, text="Shorten", command=shorten_url)
 copy_button = tk.Button(app_window, text="Copy to Clipboard", command=copy_to_clipboard, state=tk.DISABLED)
 
+# Grid System
 long_label.grid(row=0, column=0, padx=10, pady=10)
 long_entry.grid(row=0, column=1, padx=10, pady=10)
 shorten_button.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
 result_label.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
 copy_button.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
-
 app_window.mainloop()
